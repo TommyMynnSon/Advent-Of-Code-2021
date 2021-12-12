a7ac971ca19a58c0ee885725f04ce7066e1fbbcb
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-12-12 01:26:04 -0800

    Complete part 1 of day 5: hydrothermal venture

---
+++ day-5/5-hydrothermal-venture.js
@@ -0,0 +1,107 @@
+import { createInterface } from 'readline';
+import { createReadStream } from 'fs';
+
+// Keeps track of the number of hydrothermal vent lines
+// read from puzzle-input.txt.
+let numberOfHydrothermalVents = 1;
+
+// Object to act as storage for all hydrothermal vent lines
+// read from puzzle-input.txt.
+const hydrothermalVentLines = {};
+
+// Object to keep track of points that get touched by hydrothermal vent lines.
+const touchedPoints = {};
+
+// Create interface for reading data from puzzle-input.txt.
+const lineReader = createInterface({
+  input: createReadStream('./puzzle-input.txt')
+});
+
+// Read each line of puzzle-input.txt and store the coordinates 
+// of each hydrothermal vent line into the hydrothermalVentLines object.
+lineReader.on('line', (line) => {
+  const coordinatesOfEnds = [];
+
+  line.split(' -> ').forEach((coordinateOfOneEnd) => {
+    coordinatesOfEnds.push(coordinateOfOneEnd.split(',').map((number) => parseInt(number)));
+  });
+
+  hydrothermalVentLines[numberOfHydrothermalVents] = coordinatesOfEnds;
+  numberOfHydrothermalVents++;
+});
+
+lineReader.on('close', () => {
+  console.log('dangerous points:', getNumberOfDangerousPoints());
+});
+
+// Given the coordinates of a vertical hydrothermal vent line, updates
+// the touchedPoints object.
+const markPointsVertically = (hydrothermalVentLine) => {
+  if (hydrothermalVentLine[0][1] > hydrothermalVentLine[1][1]) {
+    for (let i = hydrothermalVentLine[1][1]; i <= hydrothermalVentLine[0][1]; i++) {
+      if (touchedPoints[[hydrothermalVentLine[0][0], i].toString()] === undefined) {
+        touchedPoints[[hydrothermalVentLine[0][0], i].toString()] = 1;
+      } else {
+        touchedPoints[[hydrothermalVentLine[0][0], i].toString()]++;
+      }
+    }
+  } else {
+    for (let i = hydrothermalVentLine[0][1]; i <= hydrothermalVentLine[1][1]; i++) {
+      if (touchedPoints[[hydrothermalVentLine[0][0], i].toString()] === undefined) {
+        touchedPoints[[hydrothermalVentLine[0][0], i].toString()] = 1;
+      } else {
+        touchedPoints[[hydrothermalVentLine[0][0], i].toString()]++;
+      }
+    }
+  }
+};
+
+// Given the coordinates of a horizontal hydrothermal vent line, updates
+// the touchedPoints object.
+const markPointsHorizontally = (hydrothermalVentLine) => {
+  if (hydrothermalVentLine[0][0] > hydrothermalVentLine[1][0]) {
+    for (let i = hydrothermalVentLine[1][0]; i <= hydrothermalVentLine[0][0]; i++) {
+      if (touchedPoints[[i, hydrothermalVentLine[0][1]].toString()] === undefined) {
+        touchedPoints[[i, hydrothermalVentLine[0][1]].toString()] = 1;
+      } else {
+        touchedPoints[[i, hydrothermalVentLine[0][1]].toString()]++;
+      }
+    }
+  } else {
+    for (let i = hydrothermalVentLine[0][0]; i <= hydrothermalVentLine[1][0]; i++) {
+      if (touchedPoints[[i, hydrothermalVentLine[0][1]].toString()] === undefined) {
+        touchedPoints[[i, hydrothermalVentLine[0][1]].toString()] = 1;
+      } else {
+        touchedPoints[[i, hydrothermalVentLine[0][1]].toString()]++;
+      }
+    }
+  }
+};
+
+// Logic to update the touchedPoints object with the number of times that each point gets touched by hydrothermal vent lines.
+const trackTouchedPoints = () => {
+  for (const line in hydrothermalVentLines) {
+    if (hydrothermalVentLines[line][0][0] === hydrothermalVentLines[line][1][0]) {
+      markPointsVertically(hydrothermalVentLines[line]);
+    }
+
+    if (hydrothermalVentLines[line][0][1] === hydrothermalVentLines[line][1][1]) {
+      markPointsHorizontally(hydrothermalVentLines[line]);
+    }
+  }
+};
+
+// Get the number of points that have at least two hydrothermal vet lines overlapping it.
+const getNumberOfDangerousPoints = () => {
+  trackTouchedPoints();
+
+  let numberOfDangerousPoints = 0;
+
+  for (const point in touchedPoints) {
+    if (touchedPoints[point] >= 2) {
+      numberOfDangerousPoints++;
+    }
+  }
+
+  return numberOfDangerousPoints;
+};
